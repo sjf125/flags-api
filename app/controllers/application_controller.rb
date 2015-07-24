1f426a07343cba8becf611b55c0bd226542ac11b
Antony Donovan <antony.donovan@generalassemb.ly>
2015-07-24 16:37:59 -0400

    Add routes and actions for User login and create
    Use credentials as params hash for these actions.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::API
+  before_action :authenticate
+
+  attr_reader :current_user
+
+  private
+
+  def authenticate
+    authenticate_or_request_with_http_token do |token, options|
+      @current_user = User.find_by token: token
+    end
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,24 +1,35 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :update, :destroy]
+  skip_before_action :authenticate, only: [:login, :create]
+
+  # POST /login
+  def login
+    credentials = user_credentials
+    token = User.login(credentials[:email], credentials[:password])
+    if token
+      render json: { token: token }
+    else
+      head :unauthorized
+    end
+  end
 
   # GET /users
   # GET /users.json
   def index
-    @users = User.all
+    # @users = User.all
 
-    render json: @users
+    # render json: @users
   end
 
   # GET /users/1
   # GET /users/1.json
   def show
-    render json: @user
+    # render json: @user
   end
 
   # POST /users
   # POST /users.json
   def create
-    @user = User.new(user_params)
+    @user = User.new(user_credentials)
 
     if @user.save
       render json: @user, status: :created, location: @user
@@ -30,30 +41,28 @@ class UsersController < ApplicationController
   # PATCH/PUT /users/1
   # PATCH/PUT /users/1.json
   def update
-    @user = User.find(params[:id])
+    # @user = User.find(params[:id])
 
-    if @user.update(user_params)
-      head :no_content
-    else
-      render json: @user.errors, status: :unprocessable_entity
-    end
+    # if @user.update(user_params)
+    #   head :no_content
+    # else
+    #   render json: @user.errors, status: :unprocessable_entity
+    # end
   end
 
   # DELETE /users/1
   # DELETE /users/1.json
   def destroy
-    @user.destroy
+    # @user.destroy
 
-    head :no_content
+    # head :no_content
   end
 
   private
 
-    def set_user
-      @user = User.find(params[:id])
-    end
-
-    def user_params
-      params.require(:user).permit(:email, :password, :password_confirmation)
-    end
+  def user_credentials
+    params.require(:credentials).permit(:email,
+                                        :password,
+                                        :password_confirmation)
+  end
 end
