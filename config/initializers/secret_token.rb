8da59844abcc5e2dfd3d63a48d63abc08a94089c
Antony Donovan <antony.donovan@generalassemb.ly>
2015-07-24 10:04:12 -0400

    Create default rails-api
    Generated files only.
    Using PostgreSQL as the database engine.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,19 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+
+# Although this is not needed for an api-only application, rails4 
+# requires secret_key_base or secret_token to be defined, otherwise an 
+# error is raised.
+# Using secret_token for rails3 compatibility. Change to secret_key_base
+# to avoid deprecation warning.
+# Can be safely removed in a rails3 api-only application.
+Project2Api::Application.config.secret_token = '4cab606e73bf2c5f5d5230633fa3d5ffb02dd802c418115dd3ba123ae0b138b36fd931f051893e8f0ab80147671abd881ae4bcf7914212fb496c36a9bd596268'
