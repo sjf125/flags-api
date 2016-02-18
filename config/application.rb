e8fe4f942cb2f04ec0b84755bd51c3da62bdeaca
Jeffrey Horn <jeffh@generalassemb.ly>
2016-02-18 09:58:10 -0500

    Change project name and database configuration

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,7 @@ require "action_controller/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Project2Api
+module RailsApiTemplate
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
