66341c761822669f0622dd145c2c41935fe0cf77
sjf125 <sjf125@gmail.com>
2016-05-03 12:36:05 -0400

    Add updated_at to comments serializer attrs

--- app/serializers/comment_serializer.rb
+++ app/serializers/comment_serializer.rb
@@ -1,5 +1,5 @@
 class CommentSerializer < ActiveModel::Serializer
-  attributes :id, :content, :created_at
+  attributes :id, :content, :created_at, :updated_at
   has_one :user
   has_one :flag
 end
